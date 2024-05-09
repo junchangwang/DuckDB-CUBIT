5b276a471834c00f2ccf6fba60092be5988428e5
Wei Xi <2424232508@qq.com>
2024-05-09 16:08:18 +0800

    Remove cubit-pds

--- src/include/duckdb/execution/index/cubit/CUBIT-PDS/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf build
-mkdir build
-cd build
-# cmake -Dtest=OFF -DCMAKE_BUILD_TYPE=Release ..
-cmake ..
-make -j4 VERBOSE=1
-#make test
--- src/include/duckdb/execution/index/cubit/CUBIT-PDS/src/nicolas/perf.h
+++
@@ -1,4 +0,0 @@
-
-int gen_perf_process(char *tag);
-
-int kill_perf_process(int perf_pid);
